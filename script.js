1f12ca362f84b47f29622f11c9b7a4d1be71d54e
bibi10 <bdjafalo@outlook.com>
2017-09-28 00:14:58 +0200

    update  clock

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 var date = new Date();
-
     let hr = date.getHours();
     let sec = date.getSeconds();
     let min = date.getMinutes();
-    let minPosition = (hr * 360 / 60) + (sec * (360 / 60 / 60));
-    let secPosition = (sec * 360 / 60);
-    let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+    let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+    let secPosition = (sec * 360/60);
+    let hrPosition = (hr * 360/12) + (min * (360/60)/12);
 
 function runClock() {
   
