8962dd066a65aec1808ce0cc86bc2e060f7eadb7
bibi10 <bdjafalo@outlook.com>
2017-09-27 23:37:16 +0200

    get date

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ let minPosition = 120;
 let secPosition = 10;
 let hrPososition = 253;
 
+var date = new Date();
+console.log(date);
+let hr  = date.getHours();
+let sec = date.getSeconds();
+let min = date.getMinutes;
+console.log(hr +" "+ min);
+
+
+
+
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + hrPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + hrPosition + "deg)";
