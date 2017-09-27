d5f124a07568940f71d1e2548f684f7f4051fa65
bibi10 <bdjafalo@outlook.com>
2017-09-27 23:57:09 +0200

    add time interval  to clock

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const HOURHAND   = document.querySelector("#hour");
+const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let minPosition = (hr*360/60)+(sec*(360/60/60));
-let secPosition = (sec*360/60);
-let hrPosition = (hr*360/12)+(min*(360/60)/12);
+function runClock() {
+    var date = new Date();
 
-var date = new Date();
-console.log(date);
-let hr  = date.getHours();
-let sec = date.getSeconds();
-let min = date.getMinutes();
-console.log(sec);
+    let hr = date.getHours();
+    let sec = date.getSeconds();
+    let min = date.getMinutes();
+    let minPosition = (hr * 360 / 60) + (sec * (360 / 60 / 60));
+    let secPosition = (sec * 360 / 60);
+    let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
 
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + hrPosition + "deg)";
 
+}
 
-
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + hrPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + hrPosition + "deg)";
+ var interval = setInterval(runClock, 1000);
